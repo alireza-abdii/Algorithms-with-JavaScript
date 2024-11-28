69571db19059e49c6b16a097e07199ef3c6d4258
alireza-abdii <alirezaabdichari@gmail.com>
2024-11-28 20:53:22 +0330

    improve binary-search

--- Search-Algorithms/Binary-Search.js
+++ Search-Algorithms/Binary-Search.js
@@ -1,16 +1,14 @@
 
 
 const binarySearch = (arr, target) => {
-    let sortedArray = arr.sort((a, b) => a - b)
-
     let left = 0;
-    let right = sortedArray.length - 1;
+    let right = arr.length - 1;
 
     while (left <= right) {
         let mid = Math.floor((left + right) / 2);
 
-        if (sortedArray[mid] === target) return mid;
-        if (sortedArray[mid] < target) {
+        if (arr[mid] === target) return mid;
+        if (arr[mid] < target) {
             left = mid + 1;
         } else {
             right = mid - 1;
@@ -22,4 +20,7 @@ const binarySearch = (arr, target) => {
 
 const arr = [12, 24, 34, 23, 55, 24, 23, 98, 12, 3, 7, 12, 65];
 
-console.log(binarySearch(arr, 23));
+let sortedArray = arr.sort((a, b) => a - b)
+
+console.log("Sorted Array: ", sortedArray);
+console.log(binarySearch(sortedArray, 23));

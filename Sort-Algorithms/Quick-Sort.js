61b3fa0b6af11f9b0b31d9c6b655372ef585fde2
alireza-abdii <alirezaabdichari@gmail.com>
2024-11-29 18:51:30 +0330

    complete quick-sort

--- Sort-Algorithms/Quick-Sort.js
+++ Sort-Algorithms/Quick-Sort.js
@@ -7,7 +7,7 @@ const quickSort = arr => {
     const left = [];
     const right = [];
 
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length - 1; i++) {
         if (arr[i] < pivot) {
             left.push(arr[i]);
         } else {
@@ -18,3 +18,6 @@ const quickSort = arr => {
     return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
+const arr = [12, 24, 34, 23, 55, 24, 23, 98, 12, 3, 7, 12, 65];
+
+console.log(quickSort(arr));

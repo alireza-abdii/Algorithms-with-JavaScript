a4100f47037342d8ce62db65770df813f84e2765
alireza-abdii <alirezaabdichari@gmail.com>
2024-11-29 18:59:28 +0330

    complete merge-sort

---
+++ Sort-Algorithms/Merge-Sort.js
@@ -0,0 +1,33 @@
+
+
+const mergeSort = arr => {
+    if (arr.length <= 1) return arr;
+
+    const mid = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
+
+    return merge(left, right);
+}
+
+const merge = (left, right) => {
+    const result = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            result.push(left[i]);
+            i++;
+        } else {
+            result.push(right[j]);
+            j++;
+        }
+    }
+
+    return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+const arr = [12, 24, 34, 23, 55, 24, 23, 98, 12, 3, 7, 12, 65];
+
+console.log(mergeSort(arr));

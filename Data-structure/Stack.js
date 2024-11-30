2cb06dd76a72c5db685da855d351250dfc027024
alireza-abdii <alirezaabdichari@gmail.com>
2024-11-30 23:33:37 +0330

    complete stack

--- Data-structure/Stack.js
+++ Data-structure/Stack.js
@@ -1,5 +1,51 @@
 
 
-class Stack{
-    
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(el) {
+        this.items.push(el);
+    }
+
+    pop() {
+        if (this.isEmpty()) {
+            throw new Error("Stack is empty!");
+        }
+        return this.items.pop();
+    }
+
+    peek() {
+        if (this.isEmpty()) {
+            throw new Error("Stack is empty!");
+        }
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    size() {
+        return this.items.length;
+    }
+
+    clear() {
+        this.items = [];
+    }
 }
+
+
+const stack = new Stack();
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.size());
+console.log(stack.isEmpty());
+stack.clear();
+console.log(stack.isEmpty());

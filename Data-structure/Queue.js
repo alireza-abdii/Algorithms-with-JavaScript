4b495e8744fc66548f2a28569c69bf46286b5a86
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-01 14:48:41 +0330

    improve queue code

--- Data-structure/Queue.js
+++ Data-structure/Queue.js
@@ -4,4 +4,30 @@ class Queue {
     constructor() {
         this.items = [];
     }
+
+    enqueue(el) {
+        this.items.push(el);
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            throw new Error("Queue is empty!");
+        }
+        return this.items.shift();
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    size() {
+        return this.items.length;
+    }
+
+    clear() {
+        this.items = [];
+    }
 }
+
+const queue = new Queue;
+

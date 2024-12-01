751f8d9341a50dc9a3d427109cc89f5f097144f6
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-01 14:51:34 +0330

    test queue code

--- Data-structure/Queue.js
+++ Data-structure/Queue.js
@@ -16,6 +16,13 @@ class Queue {
         return this.items.shift();
     }
 
+    front() {
+        if (this.isEmpty()) {
+            throw new Error("Queue is empty!");
+        }
+        return this.items[0];
+    }
+
     isEmpty() {
         return this.items.length === 0;
     }
@@ -31,3 +38,12 @@ class Queue {
 
 const queue = new Queue;
 
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+
+console.log(queue.front());
+console.log(queue.dequeue());
+console.log(queue.size());
+queue.clear();
+console.log(queue.isEmpty());

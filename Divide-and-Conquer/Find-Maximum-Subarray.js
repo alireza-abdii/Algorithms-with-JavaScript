501827c31736e144648078570801b42dc64b647b
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-02 14:15:36 +0330

    finish find maximum subarray code

--- Divide-and-Conquer/Find-Maximum-Subarray.js
+++ Divide-and-Conquer/Find-Maximum-Subarray.js
@@ -27,3 +27,24 @@ const findMaxCrossingSubarray = (arr, low, mid, high) => {
 
     return { maxLeft, maxRight, sum: leftSum + rightSum };
 }
+
+const findMaximumSubarray = (arr, low, high) => {
+    if (low === high) {
+        return { maxLeft: low, maxRight: high, sum: arr[low] }
+    } else {
+        const mid = Math.floor((low + high) / 2);
+
+        const leftResult = findMaxCrossingSubarray(arr,low,mid);
+        const rightResult = findMaxCrossingSubarray(arr, mid+1,high);
+        const crossResult = findMaxCrossingSubarray(arr,low, mid,high);
+
+        if(leftResult.sum >= rightResult.sum && leftResult.sum >= crossResult.sum){
+            return leftResult;
+        } else if (rightResult.sum >= leftResult.sum && rightResult.sum >= crossResult.sum) {
+            return rightResult;
+        } else {
+            return crossResult;
+        }
+    }
+}
+

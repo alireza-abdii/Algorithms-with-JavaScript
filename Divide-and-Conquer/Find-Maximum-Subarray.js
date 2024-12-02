e3d97fcc622f3408c7c381e0509180360d354e02
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-02 14:07:44 +0330

    finish findMaxCrossingSubarray helper function

--- Divide-and-Conquer/Find-Maximum-Subarray.js
+++ Divide-and-Conquer/Find-Maximum-Subarray.js
@@ -1,5 +1,29 @@
 
 
-const findMaxCrossingSubarray = (arr, low,mid, high) => {
-        
+const findMaxCrossingSubarray = (arr, low, mid, high) => {
+    let leftSum = -Infinity;
+    let sum = 0;
+    let maxLeft = mid;
+
+    for (let i = mid; i >= low; i--) {
+        sum += arr[i];
+        if (sum > leftSum) {
+            leftSum = sum;
+            maxLeft = i;
+        }
+    }
+
+    let rightSum = -Infinity;
+    sum = 0;
+    let maxRight = mid + 1;
+
+    for (let j = mid + 1; j <= high; j++) {
+        sum = arr[j];
+        if (sum > rightSum) {
+            rightSum = sum;
+            maxRight = j;
+        }
+    }
+
+    return { maxLeft, maxRight, sum: leftSum + rightSum };
 }

3529818850535155a0fb7a25855ad0560c35779f
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-03 11:34:33 +0330

    complete find min & max number in array

--- Problems/Find-Min-and-Max-in-Array.js
+++ Problems/Find-Min-and-Max-in-Array.js
@@ -1,7 +1,26 @@
 
 
-const findMinMax = arr => {
-    if(arr.length === 0 ){ 
-        return {min: null, max: null};
+const findMinMax = nums => {
+    if (nums.length === 0) {
+        return { min: null, max: null };
     }
+
+    let min = nums[0];
+    let max = nums[0];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < min) {
+            min = nums[i]
+        }
+        if (nums[i] > max) {
+            max = nums[i];
+        }
+    }
+
+    return { min, max };
 }
+
+const numbersArray = [3, 7, 1, 9, -5, 10, 0];
+const result = findMinMax(numbersArray);
+console.log(`Smallest number is ${result.min}`);
+console.log(`Largest number is ${result.max}`);

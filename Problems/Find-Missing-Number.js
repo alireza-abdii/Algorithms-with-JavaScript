191a08fd57946d145c45073f1d0a2bcd5d2550f7
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-04 09:54:11 +0330

    improve find missing number

--- Problems/Find-Missing-Number.js
+++ Problems/Find-Missing-Number.js
@@ -1,12 +1,16 @@
 
 
 const findMissingNum = arr => {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== arr[i + 1]) {
-            return arr[i] + 1;
-        }
-    }
-    return "No Missing Number :)";
+    const n = arr.length + 1;
+    const totalSum = (n * (n + 1)) / 2;
+    const actualSum = arr.reduce((sum, num) => sum + num, 0);
+    const missingNum = totalSum - actualSum;
+
+    const sortedArr = [...arr].sort((a, b) => a - b);
+    let missingIndex = sortedArr.findIndex((num, index) => num > missingNum);
+    if (missingIndex === -1) missingIndex = arr.length;
+
+    return { missingNum, missingIndex };
 }
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 10];

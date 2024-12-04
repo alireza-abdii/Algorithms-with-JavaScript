412cf92cdf23cfc490cd2b07d2a3eeded81fadcb
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-04 10:15:15 +0330

    finish find duplicates in array

--- Problems/Find-Duplicates-in-Array.js
+++ Problems/Find-Duplicates-in-Array.js
@@ -1,5 +1,30 @@
 
 
 const findDuplicates = arr => {
-    
+    const counts = {};
+    const duplicates = {};
+
+    arr.forEach(el => {
+        counts[el] = (counts[el] || 0) + 1;
+    });
+
+    for (let [key, value] of Object.entries(counts)) {
+        if (value > 1) {
+            duplicates.push({ element: +key, count: value });
+        }
+    }
+
+    const uniqueArray = arr.filter(num => counts[num] === 1);
+
+    return { duplicates, uniqueArray };
 }
+
+const stringArray = ["apple", "banana", "apple", "orange", "banana", "grape", "apple"];
+const numberArray = [1, 2, 3, 2, 4, 5, 6, 3, 3, 7, 8, 8];
+const stringResult = findAndRemoveDuplicates(stringArray);
+const numberResult = findAndRemoveDuplicates(numberArray);
+
+console.log("Duplicates Strings:", stringResult.duplicates);
+console.log("Array without duplicates strings:", stringResult.uniqueArray);
+console.log("Duplicates Numbers:", numberResult.duplicates);
+console.log("Array without duplicates numbers:", numberResult.uniqueArray);

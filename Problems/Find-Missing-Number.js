4afee9c46f26718d8274edb593ee85dc681edf14
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-04 09:48:44 +0330

    finish find missing number

--- Problems/Find-Missing-Number.js
+++ Problems/Find-Missing-Number.js
@@ -1,5 +1,13 @@
 
 
-const findMissingNum = arr = {
-    
+const findMissingNum = arr => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i + 1]) {
+            return arr[i] + 1;
+        }
+    }
+    return "No Missing Number :)";
 }
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+console.log(findMissingNum(arr));

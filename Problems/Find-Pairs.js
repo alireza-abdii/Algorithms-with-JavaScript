f07ed898682c042e5dde24436c88b77cbf3aafa5
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-05 22:05:42 +0330

    finish find pairs code

--- Problems/Find-Pairs.js
+++ Problems/Find-Pairs.js
@@ -1,5 +1,17 @@
 
 
 const findPairs = (arr, target) => {
-    
+    const seen = new Set();
+    const pairs = [];
+
+    for (const num of arr) {
+        const complement = target - num;
+        if (seen.has(complement)) {
+            pairs.push([complement, num]);
+        }
+        seen.add(num);
+    }
+
+    return pairs;
 }
+

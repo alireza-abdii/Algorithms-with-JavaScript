421a5c5dda98fc12898cb19a8d0feda942c168ce
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-06 19:40:27 +0330

    finish is prime

--- Problems/isPrime.js
+++ Problems/isPrime.js
@@ -1,5 +1,15 @@
 
 
 const isPrime = num => {
-    
+    let divisor = 2;
+
+    while (num > divisor) {
+        if (n % divisor === 0) {
+            return false
+        } else {
+            divisor++;
+        }
+    }
+    return true;
 }
+

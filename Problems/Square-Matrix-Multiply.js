c0ba9650c2e181eb6bc6bd21c13a01f32c628eba
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-08 08:57:03 +0330

    complete square matrix multiply

--- Problems/Square-Matrix-Multiply.js
+++ Problems/Square-Matrix-Multiply.js
@@ -12,3 +12,18 @@ const squareMatrixMultiply = (A, B) => {
     return result;
 }
 
+const A = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+const B = [
+    [9, 8, 7],
+    [6, 5, 4],
+    [3, 2, 1]
+];
+
+const result = squareMatrixMultiply(A, B);
+console.log("Result:");
+console.table(result);

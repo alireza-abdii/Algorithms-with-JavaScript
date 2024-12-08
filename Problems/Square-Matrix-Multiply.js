e57bc24987bec452046eeca123d4080110917140
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-08 08:54:20 +0330

    finish square matrix multiply code

--- Problems/Square-Matrix-Multiply.js
+++ Problems/Square-Matrix-Multiply.js
@@ -1,5 +1,14 @@
 
 
-const squareMatrixMultiply = (A,B) => {
-    
+const squareMatrixMultiply = (A, B) => {
+    const n = A.length;
+    const result = Array.from({ length: n }, () => Array(n).fill(0));
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            result[i][j] += A[i][j] * B[i][j];
+        }
+    }
+    return result;
 }
+

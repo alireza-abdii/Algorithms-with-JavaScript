6996605465e8eac7af43e87d1cd405323307cfcf
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-09 09:33:34 +0330

    complete dfs recursive

--- Search-Algorithms/Depth-First-Search-Recursive.js
+++ Search-Algorithms/Depth-First-Search-Recursive.js
@@ -1,5 +1,24 @@
 
 
 const dfsRecursive = (graph, start, visited = new Set()) => {
-    
+    visited.add(start);
+    console.log(start);
+
+    for (const neighbor of graph[start]) {
+        if (!visited.has(neighbor)) {
+            dfsRecursive(graph, neighbor, visited);
+        }
+    }
 }
+
+const graph = {
+    A: ["B", "C"],
+    B: ["D", "E"],
+    C: ["F"],
+    D: [],
+    E: ["F"],
+    F: []
+};
+
+console.log("DFS (Recursive):");
+dfsRecursive(graph, "A");

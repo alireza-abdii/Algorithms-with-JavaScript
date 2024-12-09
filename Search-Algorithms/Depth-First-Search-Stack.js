ab32018c38a9405915a5c127d8e1f617ec5bed09
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-09 09:40:16 +0330

    complete dfs stack

--- Search-Algorithms/Depth-First-Search-Stack.js
+++ Search-Algorithms/Depth-First-Search-Stack.js
@@ -1,5 +1,34 @@
 
 
 const dfsIterative = (graph, start) => {
-    
-}
+    const stack = [start];
+    const visited = new Set();
+
+    while (stack.length > 0) {
+        const node = stack.pop();
+
+        if (!visited.has(node)) {
+            visited.add(node);
+            console.log(node);
+
+
+            for (const neighbor of graph[node]) {
+                if (!visited.has(neighbor)) {
+                    stack.push(neighbor);
+                }
+            }
+        }
+    }
+};
+
+const graph = {
+    A: ["B", "C"],
+    B: ["D", "E"],
+    C: ["F"],
+    D: [],
+    E: ["F"],
+    F: []
+};
+
+console.log("DFS (Iterative):");
+dfsIterative(graph, "A");

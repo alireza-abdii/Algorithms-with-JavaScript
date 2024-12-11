c198a610c1bf95769ca22cf35c7e6ddb12460cf6
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-11 23:38:55 +0330

    complete bfs

--- Search-Algorithms/Breadth-First-Search.js
+++ Search-Algorithms/Breadth-First-Search.js
@@ -4,15 +4,15 @@ const bfs = (graph, start) => {
     const queue = [start];
     const visited = new Set();
 
-    while(queue.length > 0) {
+    while (queue.length > 0) {
         const node = queue.shift();
 
-        if(!visited.has(node)){
+        if (!visited.has(node)) {
             visited.add(node);
             console.log(node);
-            
-            for(const neighbor of graph[node]){
-                if(!visited.has(neighbor)){
+
+            for (const neighbor of graph[node]) {
+                if (!visited.has(neighbor)) {
                     queue.push(neighbor);
                 }
             }
@@ -20,3 +20,14 @@ const bfs = (graph, start) => {
     }
 }
 
+const graph = {
+    A: ["B", "C"],
+    B: ["D", "E"],
+    C: ["F"],
+    D: [],
+    E: ["F"],
+    F: []
+};
+
+console.log("BFS:");
+bfs(graph, "A");

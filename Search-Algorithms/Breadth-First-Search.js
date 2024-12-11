2f937207c5dcb38a89a7de501e5e1484d60cf65a
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-11 23:37:17 +0330

    finish bfs code

--- Search-Algorithms/Breadth-First-Search.js
+++ Search-Algorithms/Breadth-First-Search.js
@@ -1,5 +1,22 @@
 
 
 const bfs = (graph, start) => {
-    
+    const queue = [start];
+    const visited = new Set();
+
+    while(queue.length > 0) {
+        const node = queue.shift();
+
+        if(!visited.has(node)){
+            visited.add(node);
+            console.log(node);
+            
+            for(const neighbor of graph[node]){
+                if(!visited.has(neighbor)){
+                    queue.push(neighbor);
+                }
+            }
+        }
+    }
 }
+

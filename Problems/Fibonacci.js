6f1850e37e03a6268e7e046fe384053c8ba1ad86
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-12 01:05:45 +0330

    finish fibonacci code

--- Problems/Fibonacci.js
+++ Problems/Fibonacci.js
@@ -1,5 +1,16 @@
 
 
 const fibonacci = n => {
-    
+    if(n <= 1) return n;
+
+    const dp = Array(n+1).fill(0);
+    dp[0]=0;
+    dp[1]=1;
+
+    for(let i =2; i <=n;i++){
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+
+    return dp[n];
 }
+

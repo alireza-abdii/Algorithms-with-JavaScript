f17aaf38803da77e320b9eac0845e4160064f023
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-13 22:24:21 +0330

    start factorial code

--- Problems/Fibonacci.js
+++ Problems/Fibonacci.js
@@ -8,7 +8,7 @@ const fibonacci = n => {
     dp[1] = 1;
 
     for (let i = 2; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2]
+        dp[i] = dp[i - 1] + dp[i - 2];
     }
 
     return dp[n];

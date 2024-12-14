25a7fc04e9bbd3b19c5ff9086b492df5443476f1
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-14 14:54:26 +0330

    finish find duplicates in string code

--- Problems/Find-Duplicates-in-String.js
+++ Problems/Find-Duplicates-in-String.js
@@ -1,5 +1,15 @@
 
 
-const findDuplicates = s => {
-    
+const findDuplicates = str => {
+    const seen = new Set();
+    let result = [];
+
+    for (const char of str) {
+        if(!seen.has(char)){
+            seen.add(char);
+            result += char;
+        }
+    }
+
+    return result;
 }

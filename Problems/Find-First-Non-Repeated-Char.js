c910b729209d236a641b7adc156f79b008699e5b
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-15 20:37:30 +0330

    finish find first non repeated char code

--- Problems/Find-First-Non-Repeated-Char.js
+++ Problems/Find-First-Non-Repeated-Char.js
@@ -1,5 +1,22 @@
 
 
 const firstNonReapetedChar = str => {
-    
+    let n = str.length;
+    let char = null;
+    let count = {};
+
+    for (let i = 0; i < n; i++) {
+        char = str[i];
+
+        if (count[char]) {
+            count[char]++;
+        } else {
+            count[char] = 1;
+        }
+    }
+
+    for (const j in count) {
+        if (count[j] == 1) return j;
+    }
 }
+

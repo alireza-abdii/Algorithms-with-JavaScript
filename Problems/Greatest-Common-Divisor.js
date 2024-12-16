f8b589570d708fbb7ff45ce487e512085677eee5
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-16 22:35:33 +0330

    complete find greatest common divisor

--- Problems/Greatest-Common-Divisor.js
+++ Problems/Greatest-Common-Divisor.js
@@ -6,8 +6,8 @@ const greatestCommonDivisor = (n1, n2) => {
 
     if (n1 < 2 || n2 < 2) return 1;
 
-    while (n1 >= divisor && b >= divisor){
-        if(n1 % divisor == 0 && b% divisor == 0){
+    while (n1 >= divisor && n2 >= divisor) {
+        if (n1 % divisor == 0 && n2 % divisor == 0) {
             greatstDivisor = divisor;
         }
         divisor++;
@@ -16,3 +16,6 @@ const greatestCommonDivisor = (n1, n2) => {
     return greatstDivisor;
 }
 
+console.log(greatestCommonDivisor(14, 21));
+
+console.log(greatestCommonDivisor(69, 169));

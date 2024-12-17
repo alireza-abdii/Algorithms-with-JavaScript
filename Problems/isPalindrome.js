cbc09b9c14753dcd53897ffb95848bcd3dc0a34b
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-17 23:52:40 +0330

    complete is palindrome

--- Problems/isPalindrome.js
+++ Problems/isPalindrome.js
@@ -7,3 +7,10 @@ const isPalindrome = str => {
     return cleanedStr === reversedStr;
 }
 
+const input1 = "madam";
+const input2 = "hello";
+const input3 = "A man, a plan, a canal: Panama";
+
+console.log(`Is "${input1}" a Palindrome?`, isPalindrome(input1));
+console.log(`Is "${input2}" a Palindrome?`, isPalindrome(input2));
+console.log(`Is "${input3}" a Palindrome?`, isPalindrome(input3));

288dd338fb16b86f9985439f9af1773a38519810
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-17 23:51:34 +0330

    finish is palindrome code

--- Problems/isPalindrome.js
+++ Problems/isPalindrome.js
@@ -1,5 +1,9 @@
 
 
 const isPalindrome = str => {
-    
+    const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedStr = cleanedStr.split("").reverse().join("");
+
+    return cleanedStr === reversedStr;
 }
+

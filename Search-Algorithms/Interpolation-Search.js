a39428b69bf9eada7d3bee5f70781c98adc1a170
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-19 20:27:55 +0330

    finish interpolation search code

--- Search-Algorithms/Interpolation-Search.js
+++ Search-Algorithms/Interpolation-Search.js
@@ -1,5 +1,20 @@
 
 
 const interpolationSearch = (arr,target) => {
-    
+    let low = 0;
+    let high = arr.length - 1;
+
+    while(low <= high && target >= arr[low] && target <= arr[high]){
+        const pos = low + Math.floor(((target - arr[low]) * (high - low)) / (arr[high] - arr[low]));
+
+        if(arr[pos] === target) return pos;
+        if(arr[pos] < target) {
+            low = pos +1;
+        } else {
+            high = pos -1;
+        }
+    }
+
+    return "Not found :(";
 }
+

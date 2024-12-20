ae1c95a8b3e5387a4be20ec4387ff34e74d5658e
alireza-abdii <alirezaabdichari@gmail.com>
2024-12-20 18:47:55 +0330

    finish jump search code

--- Search-Algorithms/Jump-Search.js
+++ Search-Algorithms/Jump-Search.js
@@ -1,5 +1,23 @@
 
 
 const jumpSearch = (arr, target) => {
-    
+    const n = arr.length;
+    const step = Math.floor(Math.sqrt(n));
+    let prev = 0;
+
+    while (arr[Math.min(step, n) - 1] < target) {
+        prev = step;
+        step += Math.floor(Math.sqrt(n));
+        if (prev >= n) return "Not found :(";
+    }
+
+    while (arr[prev] < target) {
+        prev++;
+        if (prev === Math.min(step, n)) return "not found :(";
+    }
+
+    if (arr[prev] === target) return prev;
+
+    return "Not found :(";
 }
+

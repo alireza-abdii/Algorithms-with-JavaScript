686218f72a323bf1cb76dd787ece1e0843e06f2f
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-06 21:47:43 +0330

    finish matrix multiply

--- Problems/Matrix-Multiply.js
+++ Problems/Matrix-Multiply.js
@@ -1,5 +1,24 @@
 
 
 const matrixMultiply = (a, b) => {
-    
+    const rowsA = a.length;
+    const colsA = a[0].length;
+    const rowsB = b.length;
+    const colsB = b[0].length;
+
+    if (colsA !== rowsB) {
+        throw new Error("Matrix dimensions do not allow multiplication.");
+    }
+
+    const result = Array.from({ length: rowsA }, () => Array(colsB).fill(0));
+
+    for (let i = 0; i < rowsA; i++) {
+        for (let j = 0; j < colsB; j++) {
+            for (let k = 0; k < colsA; k++) {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    return result;
 }

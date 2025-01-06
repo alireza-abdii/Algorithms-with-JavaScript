57361723cdc9bded42e1b7ece6cdfa9002a5f331
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-06 21:09:21 +0330

    finish bottom-up rod cut

--- Problems/Rod-Cutting.js
+++ Problems/Rod-Cutting.js
@@ -1,5 +1,15 @@
 
 
 const rodCutting = (prices, n) => {
-    
+    const dp = Array(n + 1).fill(0);
+
+    for (let i = 1; i <= n; i++) {
+        let max = -Infinity;
+        for (let j = 1; j <= i; j++) {
+            max = Math.max(max, prices[j - 1] + dp[i - j]);
+        }
+        dp[i] = max;
+    }
+
+    return dp[n];
 }

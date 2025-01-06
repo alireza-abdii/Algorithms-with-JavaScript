44fd619df7800108b4c61e6d73f969feeb42563b
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-06 21:22:49 +0330

    complete recursive rod cut with memo

--- Problems/Rod-Cutting-Recursive.js
+++ Problems/Rod-Cutting-Recursive.js
@@ -7,10 +7,15 @@ const rodCuttingRecursive = (prices, n, memo = {}) => {
 
     let max = -Infinity;
     for (let i = 1; i <= n; i++) {
-        max = Math.max(max, prices[i - 1] + rodCuttingRecursive(prices, n - 1, memo));
+        max = Math.max(max, prices[i - 1] + rodCuttingRecursive(prices, n - i, memo));
     }
 
     memo[n] = max;
 
-    return max;
+    return memo[n];
 }
+
+const price = [2, 5, 7, 8];
+const n = 4;
+
+console.log(`Maximum profit (Recursive): ${rodCuttingRecursive(price, n)}`);

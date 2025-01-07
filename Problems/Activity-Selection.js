13afc33b50b5afc934b2bcb9dcd273d44baa7149
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-07 14:19:35 +0330

    finish activity selection

--- Problems/Activity-Selection.js
+++ Problems/Activity-Selection.js
@@ -1,5 +1,17 @@
 
 
 const activitySelection = (activities) => {
-    
-}
+    activities.sort((a, b) => a.end - b.end);
+
+    const selectedActivities = [];
+    let lastEndTime = 0;
+
+    for (const activity of activities) {
+        if (activity.start >= lastEndTime) {
+            selectedActivities.push(activity);
+            lastEndTime = activity.end;
+        }
+    }
+
+    return selectedActivities;
+};

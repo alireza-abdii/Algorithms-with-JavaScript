0edf30191c33d7fb4e8d3f6626bddab2b2d9bc92
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-07 14:20:32 +0330

    complete activity selection

--- Problems/Activity-Selection.js
+++ Problems/Activity-Selection.js
@@ -15,3 +15,19 @@ const activitySelection = (activities) => {
 
     return selectedActivities;
 };
+
+const activities = [
+    { start: 1, end: 3 },
+    { start: 2, end: 5 },
+    { start: 4, end: 6 },
+    { start: 6, end: 8 },
+    { start: 5, end: 9 },
+    { start: 8, end: 10 }
+];
+
+const result = activitySelection(activities);
+
+console.log("Selected Activities:");
+result.forEach((activity, index) =>
+    console.log(`Activity ${index + 1}: Start = ${activity.start}, End = ${activity.end}`)
+);

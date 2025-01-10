e2c73f13674c976bb6cf73e3e18613f24191078b
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-10 18:21:25 +0330

    finish epsilon greedy class

--- Problems/Epsilon-Greedy.js
+++ Problems/Epsilon-Greedy.js
@@ -4,8 +4,25 @@ class EpsilonGreedy {
     constructor(numArms, epsilon) {
         this.numArms = numArms;
         this.epsilon = epsilon;
-        this.counts = Array(numArms).fill(0);    
-        this.values = Array(numArms).fill(0);    
+        this.counts = Array(numArms).fill(0);
+        this.values = Array(numArms).fill(0);
+    }
+
+    selectArm() {
+        if (Math.random() < this.epsilon) {
+            return Math.floor(Math.random() * this.numArms);
+        } else {
+            return this.values.indexOf(Math.max(...this.values));
+        }
+    }
+
+    update(arm, reward) {
+        this.counts[arm]++;
+
+        const n = this.counts[arm];
+        const value = this.values[arm];
+
+        this.values[arm] = value + (reward - value) / n;
     }
 }
 

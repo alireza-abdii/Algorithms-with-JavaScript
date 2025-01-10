4c75fe14723bad2d269229ec49def8c11d77f99e
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-10 18:17:02 +0330

    define epsilon greedy constructor

--- Problems/Epsilon-Greedy.js
+++ Problems/Epsilon-Greedy.js
@@ -1,5 +1,11 @@
 
 
 class EpsilonGreedy {
-    
+    constructor(numArms, epsilon) {
+        this.numArms = numArms;
+        this.epsilon = epsilon;
+        this.counts = Array(numArms).fill(0);    
+        this.values = Array(numArms).fill(0);    
+    }
 }
+

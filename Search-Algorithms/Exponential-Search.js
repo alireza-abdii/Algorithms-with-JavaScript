98ed8c2fac8f1620a6e58254b370f737741ca777
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-11 14:40:21 +0330

    define binary search function

--- Search-Algorithms/Exponential-Search.js
+++ Search-Algorithms/Exponential-Search.js
@@ -1,5 +1,16 @@
 
 
 const binarySearch = (arr, target, low, high) => {
-    
+    while (low <= high) {
+        const mid = Math.floor(low + (high - low) / 2);
+
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
 }

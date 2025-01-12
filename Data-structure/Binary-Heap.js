871a4da5355cd0c09a854e546d00bfac339bc8c6
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-12 15:15:34 +0330

    finish heap class

--- Data-structure/Binary-Heap.js
+++ Data-structure/Binary-Heap.js
@@ -5,5 +5,75 @@ class MinHeap {
         this.heap = [];
     }
 
-    
+    getParentIndex(index) {
+        return Math.floor((index - 1) / 2);
+    }
+
+    getLeftChildIndex(index) {
+        return 2 * index + 1;
+    }
+
+    getRightChildIndex(index) {
+        return 2 * index + 2;
+    }
+
+    swap(index1, index2) {
+        [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.heapifyUp(this.heap.length - 1);
+    }
+
+    heapifyUp(index) {
+        while (index > 0) {
+            const parentIndex = this.getParentIndex(index);
+
+            if (this.heap[index] < this.heap[parentIndex]) {
+                this.swap(index, parentIndex);
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    extractMin() {
+        if (this.heap.length === 0) return null;
+        if (this.heap.length === 1) return this.heap.pop();
+
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.heapifyDown(0);
+
+        return min;
+    }
+
+    heapifyDown(index) {
+        while (true) {
+            const leftIndex = this.getLeftChildIndex(index);
+            const rightIndex = this.getRightChildIndex(index);
+            let smallest = index;
+
+            if (leftIndex < this.heap.length && this.heap[leftIndex] < this.heap[smallest]) {
+                smallest = leftIndex;
+            }
+
+            if (rightIndex < this.heap.length && this.heap[rightIndex] < this.heap[smallest]) {
+                smallest = rightIndex;
+            }
+
+            if (smallest !== index) {
+                this.swap(index, smallest);
+                index = smallest;
+            } else {
+                break;
+            }
+        }
+    }
+
+    peek() {
+        return this.heap.length > 0 ? this.heap[0] : null;
+    }
 }

59ba9a6548e96af285e7da7c0bab84c9afb6059f
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-14 23:24:12 +0330

    finish kmp code

--- Search-Algorithms/Knuth-Morris-Pratt.js
+++ Search-Algorithms/Knuth-Morris-Pratt.js
@@ -3,7 +3,7 @@
 const computeLPSArray = (pattern) => {
     const m = pattern.length;
     const lps = Array(m).fill(0);
-    let length = 0; 
+    let length = 0;
     let i = 1;
 
     while (i < m) {
@@ -24,6 +24,34 @@ const computeLPSArray = (pattern) => {
     return lps;
 }
 
-const KMP = (text,pattern) => {
-    
-}
+const KMP = (text, pattern) => {
+    const n = text.length;
+    const m = pattern.length;
+
+    if (m === 0) return [];
+
+    const lps = computeLPSArray(pattern);
+    const result = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < n) {
+        if (text[i] === pattern[j]) {
+            i++;
+            j++;
+        }
+
+        if (j === m) {
+            result.push(i - j);
+            j = lps[j - 1];
+        } else if (i < n && text[i] !== pattern[j]) {
+            if (j !== 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+
+    return result;
+};

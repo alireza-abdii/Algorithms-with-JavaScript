4e28baee870e3c61f026e9b7c09242cbf2024748
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-14 23:22:56 +0330

    define compute lsp array function

--- Search-Algorithms/Knuth-Morris-Pratt.js
+++ Search-Algorithms/Knuth-Morris-Pratt.js
@@ -1,5 +1,25 @@
 
 
 const computeLPSArray = (pattern) => {
-    
+    const m = pattern.length;
+    const lps = Array(m).fill(0);
+    let length = 0; 
+    let i = 1;
+
+    while (i < m) {
+        if (pattern[i] === pattern[length]) {
+            length++;
+            lps[i] = length;
+            i++;
+        } else {
+            if (length !== 0) {
+                length = lps[length - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+
+    return lps;
 }

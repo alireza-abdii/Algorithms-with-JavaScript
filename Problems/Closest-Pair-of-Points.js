87452c5a217416b5d2349705dce4c14d2d422584
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-16 22:12:27 +0330

    define bruteForceClosest function

--- Problems/Closest-Pair-of-Points.js
+++ Problems/Closest-Pair-of-Points.js
@@ -4,3 +4,19 @@ const euclideanDistance = (p1, p2) => {
     return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
 };
 
+const bruteForceClosest = (points) => {
+    let minDist = Infinity;
+    let closestPair = null;
+
+    for (let i = 0; i < points.length; i++) {
+        for (let j = i + 1; j < points.length; j++) {
+            const dist = euclideanDistance(points[i], points[j]);
+            if (dist < minDist) {
+                minDist = dist;
+                closestPair = [points[i], points[j]];
+            }
+        }
+    }
+
+    return { minDist, closestPair };
+};

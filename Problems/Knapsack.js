a16c8b88d108c32aa32b29172a2f7d23c4f74734
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-17 22:49:40 +0330

    improve knapsack function

--- Problems/Knapsack.js
+++ Problems/Knapsack.js
@@ -4,13 +4,12 @@ const knapsack = (weights, values, capacity) => {
     const n = weights.length;
     const dp = Array.from({ length: n + 1 }, () => Array(capacity + 1).fill(0));
 
-    // پر کردن جدول dp
     for (let i = 1; i <= n; i++) {
         for (let w = 1; w <= capacity; w++) {
             if (weights[i - 1] <= w) {
                 dp[i][w] = Math.max(
-                    values[i - 1] + dp[i - 1][w - weights[i - 1]], // شامل کردن آیتم i
-                    dp[i - 1][w] // شامل نکردن آیتم i
+                    values[i - 1] + dp[i - 1][w - weights[i - 1]],
+                    dp[i - 1][w]
                 );
             } else {
                 dp[i][w] = dp[i - 1][w];
@@ -18,7 +17,6 @@ const knapsack = (weights, values, capacity) => {
         }
     }
 
-    // بازسازی جواب
     let w = capacity;
     const selectedItems = [];
     for (let i = n; i > 0 && w > 0; i--) {

19cd1e9e0ee138e99a1ac4339ae8c020035a8e90
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-21 22:37:23 +0330

    define heap to graph

--- Problems/Dijkstra.js
+++ Problems/Dijkstra.js
@@ -1,3 +1,56 @@
 
 
-const 
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(node, distance) {
+        this.heap.push({ node, distance });
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[index].distance >= this.heap[parentIndex].distance) break;
+            [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+            index = parentIndex;
+        }
+    }
+
+    extractMin() {
+        if (this.heap.length === 1) return this.heap.pop();
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return min;
+    }
+
+    bubbleDown() {
+        let index = 0;
+        const length = this.heap.length;
+
+        while (true) {
+            let leftIndex = 2 * index + 1;
+            let rightIndex = 2 * index + 2;
+            let smallest = index;
+
+            if (leftIndex < length && this.heap[leftIndex].distance < this.heap[smallest].distance) {
+                smallest = leftIndex;
+            }
+            if (rightIndex < length && this.heap[rightIndex].distance < this.heap[smallest].distance) {
+                smallest = rightIndex;
+            }
+            if (smallest === index) break;
+
+            [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+            index = smallest;
+        }
+    }
+
+    isEmpty() {
+        return this.heap.length === 0;
+    }
+}

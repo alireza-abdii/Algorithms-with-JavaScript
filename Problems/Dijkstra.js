9b242078cb589f8cef608958f4095b4c0ea87ce6
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-21 22:38:57 +0330

    complete dijkstra

--- Problems/Dijkstra.js
+++ Problems/Dijkstra.js
@@ -84,3 +84,15 @@ const dijkstra = (graph, start) => {
 
     return distances;
 };
+
+const graph = {
+    A: { B: 1, C: 4 },
+    B: { A: 1, C: 2, D: 5 },
+    C: { A: 4, B: 2, D: 1 },
+    D: { B: 5, C: 1 }
+};
+
+const startNode = "A";
+const result = dijkstra(graph, startNode);
+
+console.log(`Shortest distances from ${startNode}:`, result);

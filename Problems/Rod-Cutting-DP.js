37c281d3f087fd420ca64ac9413d8e4c2e536afc
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-24 18:26:08 +0330

    improve rod cutting with dynamic programming

--- Problems/Rod-Cutting-DP.js
+++ Problems/Rod-Cutting-DP.js
@@ -1,6 +1,6 @@
 
 
-const rodCutting = (prices, n) => {
+const rodCuttingDP = (prices, n) => {
     const dp = Array(n + 1).fill(0);
 
     for (let i = 1; i <= n; i++) {
@@ -17,4 +17,4 @@ const rodCutting = (prices, n) => {
 const price = [2, 5, 7, 8];
 const n = 4;
 
-console.log(`Maximum profit: ${rodCutting(price, n)}`);
+console.log(`Maximum profit: ${rodCuttingDP(price, n)}`);

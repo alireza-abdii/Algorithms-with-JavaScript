0eca8a563a03e03d85315829658caceb646da0bb
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-24 17:39:42 +0330

    improve extended euclid code

--- Problems/Extended-Euclid.js
+++ Problems/Extended-Euclid.js
@@ -2,14 +2,11 @@
 
 const extendedEuclid = (a, b) => {
     if (b === 0) {
-        // پایه: وقتی b صفر است
         return { gcd: a, x: 1, y: 0 };
     }
 
-    // فراخوانی بازگشتی
     const result = extendedEuclid(b, a % b);
 
-    // ضرایب x و y را به‌روز می‌کنیم
     const x = result.y;
     const y = result.x - Math.floor(a / b) * result.y;
 

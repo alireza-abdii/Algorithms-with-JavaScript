fd070c879d559f3f50d31a0d0c011e36154b0d62
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-27 21:42:27 +0330

    define unionfind for kruskal

--- Problems/Kruskal.js
+++ Problems/Kruskal.js
@@ -1,3 +1,31 @@
 
 
-const 
+class UnionFind {
+    constructor(size) {
+        this.parent = Array.from({ length: size }, (_, i) => i);
+        this.rank = Array(size).fill(0);
+    }
+
+    find(u) {
+        if (this.parent[u] !== u) {
+            this.parent[u] = this.find(this.parent[u]); // مسیر فشرده‌سازی
+        }
+        return this.parent[u];
+    }
+
+    union(u, v) {
+        const rootU = this.find(u);
+        const rootV = this.find(v);
+
+        if (rootU !== rootV) {
+            if (this.rank[rootU] > this.rank[rootV]) {
+                this.parent[rootV] = rootU;
+            } else if (this.rank[rootU] < this.rank[rootV]) {
+                this.parent[rootU] = rootV;
+            } else {
+                this.parent[rootV] = rootU;
+                this.rank[rootU]++;
+            }
+        }
+    }
+}

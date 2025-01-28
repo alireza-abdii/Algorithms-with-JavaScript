da64cb8c1e31be958d49a51f6a1045d4c2f97524
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-28 23:17:55 +0330

    define build z array for z algorithm

--- Search-Algorithms/Z-Algorithm.js
+++ Search-Algorithms/Z-Algorithm.js
@@ -1,3 +1,22 @@
 
 
-const zal
+const buildZArray = (s) => {
+    const n = s.length;
+    const z = Array(n).fill(0);
+    let left = 0, right = 0;
+
+    for (let i = 1; i < n; i++) {
+        if (i <= right) {
+            z[i] = Math.min(right - i + 1, z[i - left]);
+        }
+        while (i + z[i] < n && s[z[i]] === s[i + z[i]]) {
+            z[i]++;
+        }
+        if (i + z[i] - 1 > right) {
+            left = i;
+            right = i + z[i] - 1;
+        }
+    }
+
+    return z;
+};

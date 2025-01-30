e101cfb35d0903e25a8e3b0010b0ae4b240b970f
alireza-abdii <alirezaabdichari@gmail.com>
2025-01-30 23:30:02 +0330

    define suffix array

--- Search-Algorithms/Suffix-Array-&-LCP-Array.js
+++ Search-Algorithms/Suffix-Array-&-LCP-Array.js
@@ -1,3 +1,12 @@
 
 
-const 
+const buildSuffixArray = (s) => {
+    const n = s.length;
+    const suffixes = Array.from({ length: n }, (_, i) => s.slice(i)).map((suffix, index) => ({ index, suffix }));
+
+    // مرتب‌سازی پسوندها
+    suffixes.sort((a, b) => a.suffix.localeCompare(b.suffix));
+
+    // ساخت Suffix Array
+    return suffixes.map(suffix => suffix.index);
+};
